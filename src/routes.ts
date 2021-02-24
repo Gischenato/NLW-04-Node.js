42f7222202cbafb723509a4a540df9a83915147e
Giovanni Schenato <gischenato@gmail.com>
2021-02-24 13:26:54 -0300

    criei as surveys

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express'
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from "./controllers/UserController";
 
 const router = Router()
 
+//Criando os CONTROLLERS
 const userController = new UserController()
+const surveysController = new SurveysController() 
 
+//Criando as ROTAS
 router.post("/users", userController.create)
+
+router.post("/surveys", surveysController.create)
+router.get("/surveys", surveysController.show)
+
 
 export { router }
